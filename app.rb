1c77f1538b067bc0fea899d1151d162d38f3ebe3
Johan Lindskogen <johan.lindskogen@gmail.com>
2017-09-20 21:53:27 +0200

    Cache responses for 1 hour

--- app.rb
+++ app.rb
@@ -1,11 +1,23 @@
+require 'rack/cache'
 require 'sinatra'
 require 'sinatra/json'
 
 require_relative 'lunch'
 
+use Rack::Cache
+
 set :bind, '0.0.0.0'
 
+
+helpers do
+	def	cache_today!
+		cache_control :public, max_age: 36000
+	end
+end
+
 get '/' do
+	cache_today!
+
 	if params['locale']
 		json Lunch.today_filter_locale(params['locale'])
 	else
