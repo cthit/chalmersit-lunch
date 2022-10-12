40fdd9ba1c7a0daf9b3ac6cc84097f6189f7a21a
x183 <palmoscar4@gmail.com>
2022-10-12 11:42:18 +0200

    Added lindholmen

--- src/services/chalmrest.js
+++ src/services/chalmrest.js
@@ -15,16 +15,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.fetchChalmrest = void 0;
 const axios_1 = __importDefault(require("axios"));
 const fetchChalmrest = () => __awaiter(void 0, void 0, void 0, function* () {
-    const restaurantURLs = [
+    const restaurantURLsJohanneberg = [
         "http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/21f31565-5c2b-4b47-d2a1-08d558129279/dishoccurrences" /*kårrestaurangen*/,
         "http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/b672efaf-032a-4bb8-d2a5-08d558129279/dishoccurrences" /*linsen*/,
         "http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/a7f0f75b-c1cb-4fc3-d2a6-08d558129279/dishoccurrences" /*hyllan*/,
         "http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/3ac68e11-bcee-425e-d2a8-08d558129279/dishoccurrences" /*smak*/,
         "http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/3d519481-1667-4cad-d2a3-08d558129279/dishoccurrences" /*express*/,
     ];
+    const restaurantURLsLindholmen = [
+        "http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/c74da2cf-aa1a-4d3a-9ba6-08d5569587a1/dishoccurrences" /*L's kitchen*/,
+        "http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/871c63d7-4ddb-46b8-d2a0-08d558129279/dishoccurrences" /*L's express*/,
+        "http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/c6742862-3cc5-47b1-d2a4-08d558129279/dishoccurrences" /*L's resto*/,
+        "http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/4dce0df9-c6e7-46cf-d2a7-08d558129279/dishoccurrences" /*Kokboken*/,
+    ];
     const restaurantData = [];
-    for (let i = 0; i < restaurantURLs.length; i++) {
-        const data = yield getRestaurantData(restaurantURLs[i]);
+    for (const element of restaurantURLsJohanneberg) {
+        const data = yield getRestaurantData(element, "Johanneberg");
+        if (data !== null) {
+            restaurantData.push(data);
+        }
+    }
+    for (const element of restaurantURLsLindholmen) {
+        const data = yield getRestaurantData(element, "Lindholmen");
         if (data !== null) {
             restaurantData.push(data);
         }
@@ -32,17 +44,17 @@ const fetchChalmrest = () => __awaiter(void 0, void 0, void 0, function* () {
     return restaurantData;
 });
 exports.fetchChalmrest = fetchChalmrest;
-const getRestaurantData = (url) => __awaiter(void 0, void 0, void 0, function* () {
+const getRestaurantData = (url, location) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const data = yield axios_1.default.get(url);
-        return format(data.data);
+        return format(data.data, location);
     }
     catch (error) {
         console.log(error);
         return null;
     }
 });
-const format = (json) => {
+const format = (json, location) => {
     try {
         const formatted = {
             name: json[0].mealProvidingUnit.mealProvidingUnitName,
@@ -50,7 +62,7 @@ const format = (json) => {
                 en: [],
                 sv: [],
             },
-            location: "Chalmers",
+            location: location,
         };
         json.map((item) => {
             formatted.meals.en.push({
--- src/services/chalmrest.ts
+++ src/services/chalmrest.ts
@@ -3,16 +3,28 @@ import { json } from "express/lib/response";
 import { restaurant } from "../interfaces/Restaurant";
 
 export const fetchChalmrest = async () => {
-  const restaurantURLs = [
+  const restaurantURLsJohanneberg = [
     "http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/21f31565-5c2b-4b47-d2a1-08d558129279/dishoccurrences" /*kårrestaurangen*/,
     "http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/b672efaf-032a-4bb8-d2a5-08d558129279/dishoccurrences" /*linsen*/,
     "http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/a7f0f75b-c1cb-4fc3-d2a6-08d558129279/dishoccurrences" /*hyllan*/,
     "http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/3ac68e11-bcee-425e-d2a8-08d558129279/dishoccurrences" /*smak*/,
     "http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/3d519481-1667-4cad-d2a3-08d558129279/dishoccurrences" /*express*/,
   ];
+  const restaurantURLsLindholmen = [
+    "http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/c74da2cf-aa1a-4d3a-9ba6-08d5569587a1/dishoccurrences" /*L's kitchen*/,
+    "http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/871c63d7-4ddb-46b8-d2a0-08d558129279/dishoccurrences" /*L's express*/,
+    "http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/c6742862-3cc5-47b1-d2a4-08d558129279/dishoccurrences" /*L's resto*/,
+    "http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/4dce0df9-c6e7-46cf-d2a7-08d558129279/dishoccurrences" /*Kokboken*/,
+  ];
   const restaurantData = [];
-  for (let i = 0; i < restaurantURLs.length; i++) {
-    const data = await getRestaurantData(restaurantURLs[i]);
+  for (const element of restaurantURLsJohanneberg) {
+    const data = await getRestaurantData(element, "Johanneberg");
+    if (data !== null) {
+      restaurantData.push(data);
+    }
+  }
+  for (const element of restaurantURLsLindholmen) {
+    const data = await getRestaurantData(element, "Lindholmen");
     if (data !== null) {
       restaurantData.push(data);
     }
@@ -20,17 +32,17 @@ export const fetchChalmrest = async () => {
   return restaurantData;
 };
 
-const getRestaurantData = async (url: string) => {
+const getRestaurantData = async (url: string, location: string) => {
   try {
     const data = await axios.get(url);
-    return format(data.data);
+    return format(data.data, location);
   } catch (error) {
     console.log(error);
     return null;
   }
 };
 
-const format = (json) => {
+const format = (json, location: string) => {
   try {
     const formatted: restaurant = {
       name: json[0].mealProvidingUnit.mealProvidingUnitName,
@@ -38,7 +50,7 @@ const format = (json) => {
         en: [],
         sv: [],
       },
-      location: "Chalmers",
+      location: location,
     };
     (json as any).map((item: any) => {
       formatted.meals.en.push({
